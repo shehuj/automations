a09b471e641f6d83bb9921bec9ac29c2b1dde01b
Jenom <119628596+shehuj@users.noreply.github.com>
2025-08-19 21:14:18 -0500

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-apt-get update -y
-apt-get install -y apache2
-systemctl enable apache2
-systemctl start apache2
-echo "<h1> THIS IS LUIT - RED TEAM! </h1>" > /var/www/html/index.html
+sudo apt-get update -y
+sudo apt-get install -y apache2
+sudo systemctl enable apache2
+sudo systemctl start apache2
+echo "<h1> WELCOME TO LUIT - THIS IS RED TEAM! </h1>" > /var/www/html/index.html
